28a8b08b93818536767f710d1ee072bef5d769c0
likhith-7 <67787556+likhith-7@users.noreply.github.com>
2023-12-14 13:16:21 +0530

    Add files via upload

---
+++ logging_package/handlers.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import datetime
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+LOGS_DIR = 'C:\\Users\\Likhith.gowda\\Desktop\\logsss'
+
+class LevelFilter(logging.Filter):
+    def __init__(self, level):
+        self.level = level
+
+    def filter(self, record):
+        return record.levelno <= self.level
+
+
+class DateRotatingFileHandler(TimedRotatingFileHandler):
+    def __init__(self, filename, *args, **kwargs):
+        today = datetime.now().strftime('%Y-%m-%d')
+        log_folder = os.path.join(LOGS_DIR, today)
+        if not os.path.exists(log_folder):
+            os.makedirs(log_folder)
+        filename = os.path.join(log_folder, filename)
+        super().__init__(filename, *args, **kwargs)
+
+def cleanup_old_log_folders():
+    log_folders = sorted(os.listdir(LOGS_DIR))
+    if len(log_folders) > 5:
+        folders_to_delete = log_folders[:-5]  # Keep the 5 most recent folders
+        for folder in folders_to_delete:
+            folder_path = os.path.join(LOGS_DIR, folder)
+            if os.path.isdir(folder_path):
+                try:
+                    # Delete the contents within the folder before removing it
+                    for root, dirs, files in os.walk(folder_path):
+                        for file in files:
+                            os.remove(os.path.join(root, file))
+                    os.rmdir(folder_path)  # Remove the directory
+                except OSError as e:
+                    print(f"Error while deleting folder {folder_path}: {e}")
+                    
+                    
+cleanup_old_log_folders()
---
+++ logging_package/logging_config.py
@@ -0,0 +1,69 @@
+import logging
+from handlers import LevelFilter, DateRotatingFileHandler
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'debug_file': {
+            'level': 'DEBUG',
+            'class': 'handlers.DateRotatingFileHandler',
+            'filename': 'debug.log',
+            'when': 'midnight',
+            'interval': 1,
+            'formatter': 'verbose',
+            'filters': ['debug_filter']
+        },
+        'error_file': {
+            'level': 'ERROR',
+            'class': 'handlers.DateRotatingFileHandler',
+            'filename': 'error.log',
+            'when': 'midnight',
+            'interval': 1,
+            'formatter': 'verbose',
+            'filters': ['error_filter']
+        },
+        'warning_file': {
+            'level': 'WARNING',
+            'class': 'handlers.DateRotatingFileHandler',
+            'filename': 'warning.log',
+            'when': 'midnight',
+            'interval': 1,
+            'formatter': 'verbose',
+            'filters': ['warning_filter']
+        },
+        'info_file': {
+            'level': 'INFO',
+            'class': 'handlers.DateRotatingFileHandler',
+            'filename': 'info.log',
+            'when': 'midnight',
+            'interval': 1,
+            'formatter': 'verbose',
+            'filters': ['info_filter']
+        },
+        'console': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format' : '{asctime}:{levelname} - {name} {module}.py (line {lineno:d}). {message}',
+            'style' : "{",
+        }
+    },
+    'filters': {
+        'debug_filter': {'()': LevelFilter, 'level': logging.DEBUG},
+        'error_filter': {'()': LevelFilter, 'level': logging.ERROR},
+        'warning_filter': {'()': LevelFilter, 'level': logging.WARNING},
+        'info_filter': {'()': LevelFilter, 'level': logging.INFO},
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['debug_file', 'error_file', 'warning_file', 'info_file', 'console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
